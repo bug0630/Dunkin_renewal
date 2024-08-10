334e37b05441e67909d6e4f2ac27e7ef49c1040a
bug0630 <bugbdh@gmail.com>
2024-08-10 17:15:43 +0900

    메인 슬라이드

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const { AutoPlay, Pagination } = window.Flicking.Plugins;
 
+const m_flicking = new Flicking("#m-carousel", {
+  align: "center",
+  circular: true,
+  renderOnlyVisible: true,
+  deceleration: 0.5,
+});
 const flicking = new Flicking("#carousel", {
   align: "center",
   circular: true,
   renderOnlyVisible: true,
   deceleration: 0.5,
 });
-
 const flicking2 = new Flicking("#carousel2", {
   align: "center",
   circular: true,
   renderOnlyVisible: true,
   deceleration: 0.5,
 });
-
+const autoPlay = new AutoPlay({
+  duration: 4000,
+  direction: "NEXT",
+  stopOnHover: true,
+});
 const autoPlayPlugin = new AutoPlay({
   duration: 4000,
   direction: "NEXT",
@@ -25,7 +34,9 @@ const autoPlayPlugin2 = new AutoPlay({
   direction: "NEXT",
   stopOnHover: true,
 });
-
+const page = new Pagination({
+  type: "bullet",
+});
 const pagi = new Pagination({
   type: "bullet",
 });
@@ -34,5 +45,6 @@ const pagi2 = new Pagination({
 });
 
 // 플러그인 추가
+m_flicking.addPlugins(autoPlay, page);
 flicking.addPlugins(autoPlayPlugin, pagi);
 flicking2.addPlugins(autoPlayPlugin2, pagi2); // flicking2에도 Pagination 추가
