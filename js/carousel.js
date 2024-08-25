2dd984cfe6e81e6550f863b5871a5287c6d2446b
bug0630 <bugbdh@gmail.com>
2024-08-25 18:06:07 +0900

    resize

--- js/carousel.js
+++ js/carousel.js
@@ -17,26 +17,17 @@ function createFlicking(selector, autoPlayDuration = 4000) {
   flickingInstance.addPlugins(autoPlayPlugin);
 
   // 창 크기에 따라 Pagination 플러그인 조건부 추가
-  if (window.innerWidth < 1444) {
-    const paginationPlugin = new Pagination({
-      type: "bullet",
-    });
-    flickingInstance.addPlugins(paginationPlugin);
-  }
+
+  const paginationPlugin = new Pagination({
+    type: "bullet",
+  });
+  flickingInstance.addPlugins(paginationPlugin);
 
   return flickingInstance;
 }
 
 function responsiveFlicking(selector, autoPlayDuration = 4000) {
-  let paginationPlugin;
-
   const flickingInstance = new Flicking(selector, {
-    align:
-      window.innerWidth < 769
-        ? "prev"
-        : window.innerWidth < 978
-        ? "prev"
-        : "center",
     bound: true,
     circular: true,
     renderOnlyVisible: true,
@@ -54,39 +45,12 @@ function responsiveFlicking(selector, autoPlayDuration = 4000) {
 
   flickingInstance.addPlugins(autoPlayPlugin);
 
-  function updatePlugins() {
-    let newAlign;
-
-    if (window.innerWidth < 769) {
-      newAlign = "prev";
-    } else if (window.innerWidth < 1024) {
-      newAlign = "center";
-    } else {
-      newAlign = "center";
-    }
-
-    // align 값이 변경되었는지 확인하고 업데이트
-    if (flickingInstance.align !== newAlign) {
-      flickingInstance.align = newAlign;
-      flickingInstance.resize();
-    }
-
-    // 기존의 Pagination 플러그인이 있으면 제거
-    if (paginationPlugin) {
-      flickingInstance.removePlugins(paginationPlugin);
-    }
-
-    // 창 크기에 따라 Pagination 플러그인 조건부 추가
-    if (window.innerWidth < 1444) {
-      paginationPlugin = new Pagination({
-        type: "bullet",
-      });
-      flickingInstance.addPlugins(paginationPlugin);
-    }
+  if (window.innerWidth < 1444) {
+    const paginationPlugin = new Pagination({
+      type: "bullet",
+    });
+    flickingInstance.addPlugins(paginationPlugin);
   }
-
-  // 초기 실행
-  updatePlugins();
 
   return flickingInstance;
 }
